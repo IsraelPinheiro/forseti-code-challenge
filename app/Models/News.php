0de1bf499dbbc047974de4aa93dd85376ce08603
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-28 12:23:00 -0300

    Add HasUUID

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasUUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUUID;
 
     protected $fillable = [
         'headline',
