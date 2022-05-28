6e2eaa640a4288c44643984454c0b98422e75948
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-28 12:21:58 -0300

    Add softDeletes

--- database/migrations/2022_05_28_144404_create_tags_table.php
+++ database/migrations/2022_05_28_144404_create_tags_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->uuid();
             $table->string('tag');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
