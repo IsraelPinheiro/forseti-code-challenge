51f579f29970de3dc14849320e73b20cad8e6641
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-28 12:19:22 -0300

    Add fillable and belongsToMany relation to News

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tag',
+    ];
+
+    /**
+     * The News that were tagged with this Tag.
+     */
+    public function news()
+    {
+        return $this->belongsToMany(News::class);
+    }
 }
