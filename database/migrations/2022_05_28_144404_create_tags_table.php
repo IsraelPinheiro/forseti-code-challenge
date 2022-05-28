531f773541db0e8ffe71740f89a1feba1f360aa9
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-28 12:16:02 -0300

    Structure for tags table

--- database/migrations/2022_05_28_144404_create_tags_table.php
+++ database/migrations/2022_05_28_144404_create_tags_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
+            $table->string('tag');
             $table->timestamps();
         });
     }
