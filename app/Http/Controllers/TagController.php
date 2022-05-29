9493b843b337864568e4f5f318bee8b42859a41d
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-29 12:02:14 -0300

    Add index, store, show and destroy logic

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,62 +3,123 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index():JsonResponse
     {
-        //
+        try {
+            $tags = Tag::all();
+            return $this->success($tags);
+        } catch (Exception $exception) {
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request):JsonResponse
     {
-        //
+        try {
+            $request->validate([
+                'tag' => ['required', 'string', 'min:3', Rule::unique('tags')]
+            ]);
+            
+            $tag = Tag::create([
+                'tag' => $request->tag
+            ]);
+            
+            return $this->success($tag);
+        } catch (Exception $exception) {
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @param  string $uuid
+     * @return JsonResponse
      */
-    public function show(Tag $tag)
+    public function show(Request $request, string $uuid):JsonResponse
     {
-        //
-    }
+        try {
+            $request->merge([
+                'uuid' => $uuid
+            ])->validate([
+                'uuid' => ['required', 'uuid', 'exists:tags,uuid']
+            ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tag $tag)
-    {
-        //
+            $tag = Tag::where('uuid',$uuid)->firstOrFail();
+            return $this->success($tag);
+        } catch (Exception $exception) {
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @param  string $uuid
+     * @return JsonResponse
      */
-    public function destroy(Tag $tag)
+    public function destroy(Request $request, string $uuid):JsonResponse
     {
-        //
+        try {
+            $request->merge([
+                'uuid' => $uuid
+            ])->validate([
+                'uuid' => ['required', 'uuid', 'exists:tags,uuid']
+            ]);
+
+            $tag = Tag::where('uuid',$uuid)->firstOrFail();
+            $tag->delete();
+            return $this->success($tag, 'Deleted');
+        } catch (Exception $exception){
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 }
