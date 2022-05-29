4cbe81a2721c586a24ce2b7760486be0ceceb9bb
Israel R Pinheiro <israel.pinheiro@live.com>
2022-05-29 12:02:37 -0300

    Add index, show and destroy logic

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\News;
+use App\Models\Tag;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
@@ -10,55 +13,109 @@ class NewsController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @return JsonResponse
      */
-    public function index()
+    public function index(Request $request):JsonResponse
     {
-        //
-    }
+        try {
+            $request->validate([
+                'limit' => ['sometimes', 'required', 'numeric', 'min:1'],
+                'offset' => ['sometimes', 'required', 'numeric', 'min:1'],
+                'date_to' => ['sometimes', 'required', 'date', 'before_or_equal:today'],
+                'date_from' => ['sometimes', 'required', 'date', 'before_or_equal:date_to'],
+                'tags' => ['sometimes', 'required', 'array', 'min:1']
+            ]);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+            if($request->has('tags')){
+                $tags = Tag::whereIn('tag', $request->tags)->pluck('id');
+            }
+            
+            $news = News::when($request->has('offset'), function($query) use($request){
+                $query->skip($request->offset);
+            })->when($request->has('limit'), function($query) use($request){
+                $query->take($request->limit);
+            })->when($request->has('date_from'), function($query) use($request){
+                $query->where('created_at','>=',$request->date_from);
+            })->when($request->has('date_to'), function($query) use($request){
+                $query->where('created_at','<=',$request->date_to);
+            })->when($tags, function($query) use($tags){
+                $query->whereHas('tags', function($query) use($tags){
+                    $query->whereIn('id', $tags);
+                });
+            })->get();
+
+            return $this->success($news);
+        } catch (Exception $exception) {
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\News  $news
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @param  string $uuid
+     * @return JsonResponse
      */
-    public function show(News $news)
+    public function show(Request $request, string $uuid):JsonResponse
     {
-        //
-    }
+        try {
+            $request->merge([
+                'uuid' => $uuid
+            ])->validate([
+                'uuid' => ['required', 'uuid', 'exists:news,uuid']
+            ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\News  $news
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, News $news)
-    {
-        //
+            $news = News::where('uuid',$uuid)->firstOrFail();
+            return $this->success($news);
+        } catch (Exception $exception) {
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\News  $news
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @param  string $uuid
+     * @return JsonResponse
      */
-    public function destroy(News $news)
+    public function destroy(Request $request, string $uuid):JsonResponse
     {
-        //
+        try {
+            $request->merge([
+                'uuid' => $uuid
+            ])->validate([
+                'uuid' => ['required', 'uuid', 'exists:news,uuid']
+            ]);
+
+            $news = News::where('uuid',$uuid)->firstOrFail();
+            $news->delete();
+            return $this->success($news, 'Deleted');
+        } catch (Exception $exception){
+            return $this->error(
+                $exception->getMessage(),
+                [
+                    'exception_code' => $exception->getCode(),
+                    'exception_type' => gettype($exception),
+                    'exception_trace' => $exception->getTraceAsString()
+                ]
+            );
+        }
     }
 }
